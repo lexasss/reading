c712260d99ca1418a3b737ef816bc119eabef98e
oleg.spakov <oleg.spakov@uta.fi>
2016-04-12 11:26:52 +0300

    fix in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/build'));
 
 app.get('/', function(request, response) {
-  response.render('index.html');
+  response.sendFile('index.html');
 });
 
 app.listen(app.get('port'), function() {
